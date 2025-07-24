03640c2553f5fb6877fc1878da1029332fc90905
Samuel Oliveira <samueloliveirasantos003@gmail.com>
2025-07-24 02:01:03 -0300

    styles: melhora tela de login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -25,9 +25,19 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+    <div className="min-h-screen flex items-center justify-center bg-white">
+       <div className="flex flex-col items-center mb-18 my-10 mr-25">
+        <h1 className="text-2xl font-bold text-center text-gray-600 mb-8">
+          Sistema de Gestão de Medicamentos
+        </h1>
+        <img 
+          src="/medico.png" 
+          alt="Médico" 
+          className="w-70 h-auto" 
+        />
+      </div>
       <div className="bg-white p-8 rounded shadow-md w-96">
-        <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
+        <h2 className="text-2xl font-bold mb-6 text-center text-gray-600">Login</h2>
         {error && <p className="text-red-500 mb-4">{error}</p>}
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
@@ -37,7 +47,7 @@ export default function LoginPage() {
             <input
               type="email"
               id="email"
-              className="w-full px-3 py-2 border rounded"
+              className="w-full px-3 py-2 border rounded text-gray-700"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
@@ -50,7 +60,7 @@ export default function LoginPage() {
             <input
               type="password"
               id="password"
-              className="w-full px-3 py-2 border rounded"
+              className="w-full px-3 py-2 border rounded text-gray-700"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
@@ -58,7 +68,7 @@ export default function LoginPage() {
           </div>
           <button
             type="submit"
-            className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
+            className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 cursor-pointer"
           >
             Entrar
           </button>
@@ -67,7 +77,7 @@ export default function LoginPage() {
           <a href="/cadastro" className="text-blue-500 hover:underline">
             Criar conta
           </a>
-          <span className="mx-2">|</span>
+          <span className="mx-2 text-gray-700">|</span>
           <a href="/recuperar-senha" className="text-blue-500 hover:underline">
             Recuperar senha
           </a>
