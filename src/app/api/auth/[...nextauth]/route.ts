63f360f2a85e669d6d5b56012c5a94850575cc7a
Samuel Oliveira <samueloliveirasantos003@gmail.com>
2025-07-30 23:34:12 -0300

    fix: ajusta api

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
-import NextAuth, { NextAuthOptions, SessionStrategy } from "next-auth";
+import NextAuth from "next-auth";
+import { SessionStrategy } from "next-auth";
 
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaClient } from "@prisma/client";
