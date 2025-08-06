527e3b15733938883dabf8db2ed0bdcc7126d467
Isadora Paz <mibp@ic.ufal.br>
2025-08-06 16:51:38 -0300

    rename component

--- src/app/(auth)/dashboard/page.tsx
+++ src/app/(auth)/dashboard/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-import AddMedicineForm from "@/components/MedicineAccordion";
+import MedicineAccordion from "@/components/MedicineAccordion";
 import MedicineList from "@/components/MedicineList";
 
 export default function DashboardPage() {
   return (
     <main className="flex flex-col items-center justify-center gap-8">
-      <AddMedicineForm />
+      <MedicineAccordion />
       <MedicineList />
     </main>
   );
